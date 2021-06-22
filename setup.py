88069c07aacd135717c0790d4f2a56bf40081d15
Chris Lieb <clieb@bitsighttech.com>
2021-06-22 15:07:27 -0400

    Change version reported by setup.py to *-bs1

--- setup.py
+++ setup.py
@@ -10,13 +10,7 @@ version = re.compile(r'VERSION\s*=\s*\((.*?)\)')
 
 def get_package_version():
     "returns package version without importing it"
-    base = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(base, "flower/__init__.py")) as initf:
-        for line in initf:
-            m = version.match(line.strip())
-            if not m:
-                continue
-            return ".".join(m.groups()[0].split(", "))
+    return "1.0.1-bs1"
 
 
 def get_requirements(filename):
